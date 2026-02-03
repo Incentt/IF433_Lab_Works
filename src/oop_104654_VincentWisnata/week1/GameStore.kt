49e99150ee351d9bd7e9a9e6dc72c35df39cfc13
Vincent Wisnata <81563427+Incentt@users.noreply.github.com>
2026-02-03 18:43:26 +0700

    week01: (tugas 2) implement discount logic

--- src/oop_104654_VincentWisnata/week1/GameStore.kt
+++ src/oop_104654_VincentWisnata/week1/GameStore.kt
@@ -1,6 +1,14 @@
 package oop_104654_VincentWisnata.week1
 
 fun main() {
-   val gameTitle = "Nama Game";
+    val gameTitle = "Nama Game";
     val price = 100000;
+}
+
+fun calculateDiscount(price: Int): Int{
+    if (price > 500000){
+        return  (price *20/100)
+    }else {
+        return (price *10/100)
+    }
 }
