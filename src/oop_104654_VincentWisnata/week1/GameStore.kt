3000a3b8ad60db48ea2e0a1e0716bd596dc43dec
Vincent Wisnata <81563427+Incentt@users.noreply.github.com>
2026-02-03 18:52:03 +0700

    week01: (tugas 2) implement receipt printing

--- src/oop_104654_VincentWisnata/week1/GameStore.kt
+++ src/oop_104654_VincentWisnata/week1/GameStore.kt
@@ -1,8 +1,9 @@
 package oop_104654_VincentWisnata.week1
 
 fun main() {
-    val gameTitle = "Nama Game";
+    val gameTitle = "John Wick";
     val price = 100000;
+    printReceipt(title = gameTitle, price = price)
 }
 
 fun calculateDiscount(price: Int): Int{
@@ -11,4 +12,13 @@ fun calculateDiscount(price: Int): Int{
     }else {
         return (price *10/100)
     }
+}
+//Note dikarenakan output juga meminta harga original, saya ganti final price menjadi price - Vincent
+fun printReceipt(title: String, price: Int){
+    println("============================")
+    println("Nama Game: $title")
+    println("Harga Produk: $price")
+    println("Diskon: ${calculateDiscount(price)}")
+    println("Harga Produk setelah diskon: ${price - calculateDiscount(price)}")
+    println("============================")
 }
