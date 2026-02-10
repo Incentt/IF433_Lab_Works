a7d123657f0c857d1f4d4b581bb8869f59b2ff55
Vincent Wisnata <81563427+Incentt@users.noreply.github.com>
2026-02-10 15:27:50 +0700

    week02: implement selection logic for constructors

--- src/oop_104654_VincentWisnata/week2/student.kt
+++ src/oop_104654_VincentWisnata/week2/student.kt
@@ -11,7 +11,7 @@ class Student(
         if(nim.length != 5){
             println("NIM  ${nim} Tidak valid, NIM berisi 5 karakter")
         }else{
-            print("LOG: Objek Student $name berhasil dialokasikan ke dalam memori")
+            println("LOG: Objek Student $name berhasil dialokasikan ke dalam memori")
         }
     }
     constructor(name: String, nim: String, gpa: Double = 0.0): this(name, nim, gpa, "Non-Matriculated"){
@@ -27,14 +27,32 @@ fun main(){
     print("Masukan Nim (WAJIB 5 KARAKTER): ")
     val nim = scanner.next();
 
+    print("Pilih Jalur (1. Reguler, 2. Umum): ")
+    val type = scanner.nextInt();
     scanner.nextLine();
-    if (nim.length != 5){
-        println("ERROR: Pendaftaran dibatalkan, NIM harus 5 KARAKTER");
-    }else{
+
+    if (type == 1){
+        print("Masukan Jurusan: ")
         print("Masukan Jurusan : ")
         val major = scanner.nextLine();
 
         val s1 = Student(name, nim)
-        println("Status Pendaftaran selesai.")
+        println("Terdaftar di: ${s1.major} dengan GPA awal ${s1.gpa}")
+    } else if (type == 2){
+        val s2 = Student(nim, nim)
+        println("Terdaftar di: ${s2.major} dengan GPA awal ${s2.gpa}")
+    }else{
+        println("Pilihan ngawur, pendaftaran batal!!!!")
     }
+
+//    scanner.nextLine();
+//    if (nim.length != 5){
+//        println("ERROR: Pendaftaran dibatalkan, NIM harus 5 KARAKTER");
+//    }else{
+//        print("Masukan Jurusan : ")
+//        val major = scanner.nextLine();
+//
+//        val s1 = Student(name, nim)
+//        println("Status Pendaftaran selesai.")
+//    }
 }
