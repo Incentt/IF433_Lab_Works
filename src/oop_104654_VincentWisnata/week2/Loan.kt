ee7fbc9626658ff0ffb216f89d67c437c1476477
Vincent Wisnata <81563427+Incentt@users.noreply.github.com>
2026-02-10 15:39:23 +0700

    week02: (task 1) implement library input system

--- src/oop_104654_VincentWisnata/week2/Loan.kt
+++ src/oop_104654_VincentWisnata/week2/Loan.kt
@@ -1,7 +1,14 @@
 package oop_104654_VincentWisnata.week2
+import java.util.Scanner
 
 class Loan(var bookTitle: String, var borrower: String, var loanDuration: Int = 1) {
 
+    init {
+        println("--- DETAIL PEMINJAMAN ---")
+        println("Judul buku: $bookTitle")
+        println("Nama Peminjam: $borrower")
+        println("Denda pinjaman: ${calculateFine()}")
+    }
     fun calculateFine(): Int{
         var denda: Int = 0;
         if (loanDuration > 3){
@@ -14,5 +21,18 @@ class Loan(var bookTitle: String, var borrower: String, var loanDuration: Int = 
 }
 
 fun main() {
-   
+    val scanner = Scanner(System.`in`)
+    println("--- APLIKASI LIBRARY UMN ---")
+    print("Masukan Nama Buku : ")
+    val bookName = scanner.nextLine();
+    print("Masukan Nama Peminjam: ")
+    val borrowerName = scanner.nextLine();
+    print("Masukan Lama Pinjam: ")
+    var loanDuration = scanner.nextInt()
+    scanner.nextLine();
+
+    if (loanDuration < 0){
+        loanDuration = 1
+    }
+    var bukuPinjaman = Loan(bookName, borrowerName, loanDuration)
 }
