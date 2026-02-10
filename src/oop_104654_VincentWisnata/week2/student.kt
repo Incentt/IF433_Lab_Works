2bbcbb90f50fc6b178e202ba72fbcbe7413474d1
Vincent Wisnata <81563427+Incentt@users.noreply.github.com>
2026-02-10 15:12:33 +0700

    week02: implement user input and pre-validation

--- src/oop_104654_VincentWisnata/week2/student.kt
+++ src/oop_104654_VincentWisnata/week2/student.kt
@@ -1,15 +1,36 @@
 package oop_104654_VincentWisnata.week2
+import java.util.Scanner
 
-class student(
+class Student(
     val name: String,
     val nim: String,
     var major: String,
 ){
 init {
     if(nim.length != 5){
-        println("NIM  ${nim} Tidak valid")
+        println("NIM  ${nim} Tidak valid, NIM berisi 5 karakter")
     }else{
         print("LOG: Objek Student $name berhasil dialokasikan ke dalam memori")
     }
 }
+}
+fun main(){
+    val scanner = Scanner(System.`in`)
+    println("--- APLIKASI PMB UMN ---")
+    print("Masukan Nama : ")
+    val name = scanner.nextLine();
+
+    print("Masukan Nim (WAJIB 5 KARAKTER): ")
+    val nim = scanner.next();
+
+    scanner.nextLine();
+    if (nim.length != 5){
+        println("ERROR: Pendaftaran dibatalkan, NIM harus 5 KARAKTER");
+    }else{
+        print("Masukan Jurusan : ")
+        val major = scanner.nextLine();
+
+        val s1 = Student(name, nim, major)
+        println("Status Pendaftaran selesai.")
+    }
 }
