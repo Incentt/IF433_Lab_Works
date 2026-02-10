60b262e3a5c0cfe16d2433cf5a7b443d8ee9b808
Vincent Wisnata <81563427+Incentt@users.noreply.github.com>
2026-02-10 16:29:47 +0700

    week02: (task 2) implement the game

--- src/oop_104654_VincentWisnata/week2/Hero.kt
+++ src/oop_104654_VincentWisnata/week2/Hero.kt
@@ -1,4 +1,5 @@
 package oop_104654_VincentWisnata.week2
+import java.util.Scanner
 
 class Hero(var name: String, var baseDamage: Int, var hp: Int = 100){
 
@@ -22,5 +23,43 @@ class Hero(var name: String, var baseDamage: Int, var hp: Int = 100){
 }
 
 fun main(){
-  
+    var enemyHP = 100;
+
+    var scanner = Scanner(System.`in`)
+    println("Masukan Nama Hero: ")
+    var heroName = scanner.nextLine()
+    println("Masukan Base Damage: ")
+    var baseDamage = scanner.nextInt()
+    scanner.nextLine()
+
+    var hero = Hero(heroName, baseDamage)
+
+    while (hero.isAlive()) {
+        println("Pilih Menu: 1. Menyerang, 2. Kabur")
+        var menu = scanner.nextInt();
+        scanner.nextLine()
+        if (menu == 1){
+            println("Menyerang")
+            enemyHP -= hero.baseDamage;
+            println("Damage: ${hero.baseDamage}, sisa health musuh: ${enemyHP}")
+            if (enemyHP > 0){
+                val enemyDamage = (10..20).random()
+                hero.takeDamage(enemyDamage)
+            }
+        }else if (menu ==2 ){
+            println("Hero Kabur");
+        }else {
+            println("Error menu tidak ada");
+        }
+    }
+    var winner = "";
+    if (enemyHP > hero.hp ){
+        winner = "Enemy"
+    }else if (hero.hp > enemyHP){
+        winner = "Hero"
+    }else{
+        print("Error");
+        return;
+    }
+    println("Pemenangnya adalah: $winner")
 }
