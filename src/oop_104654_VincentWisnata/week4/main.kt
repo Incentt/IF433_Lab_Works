c65dfe5bc63feb4dca10464423ec117794532652
Vincent Wisnata <81563427+Incentt@users.noreply.github.com>
2026-02-24 14:46:36 +0700

    week04: (task 1) test ElectricCar in main

--- src/oop_104654_VincentWisnata/week4/main.kt
+++ src/oop_104654_VincentWisnata/week4/main.kt
@@ -5,10 +5,16 @@ fun main(){
     val generalVehicle = Vehicle("Sepedha Onthel")
     generalVehicle.honk()
     generalVehicle.accelerate()
-    
+
     println("------ Testing Car -----");
     val myCar = Car("Toyota",  4)
     myCar.openTrunk()
     myCar.honk()
     myCar.accelerate()
+
+    println("------ Testing Electric -----");
+    val electroCar = ElectricCar("Tesla",  4, 1000)
+    electroCar.openTrunk()
+    electroCar.honk()
+    electroCar.accelerate()
 }
