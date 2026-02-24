c99d688d454e7507fef636c2f73ecad943398960
Vincent Wisnata <81563427+Incentt@users.noreply.github.com>
2026-02-24 14:57:06 +0700

    week04: (task 2) test Employee hierarchy in main

--- src/oop_104654_VincentWisnata/week4/main.kt
+++ src/oop_104654_VincentWisnata/week4/main.kt
@@ -17,4 +17,12 @@ fun main(){
     electroCar.openTrunk()
     electroCar.honk()
     electroCar.accelerate()
+
+    val Man = Manager("eric", 10000)
+    Man.work()
+    println("Bonus Manager: ${Man.calculateBonus()}")
+
+    val Dev = Developer("vincent", 10000, "kotlin")
+    Dev.work()
+    println("Bonus Developer: ${Dev.calculateBonus()}")
 }
