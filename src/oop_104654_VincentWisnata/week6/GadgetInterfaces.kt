0cc1714d7d339f10d6e5a6d082dfad637efa6d89
Vincent Wisnata <81563427+Incentt@users.noreply.github.com>
2026-03-10 14:24:18 +0700

    week06: create Camera and Phone interfaces with default methods

---
+++ src/oop_104654_VincentWisnata/week6/GadgetInterfaces.kt
@@ -0,0 +1,12 @@
+package oop_104654_VincentWisnata.week6
+
+interface Camera{
+    fun turnOn(){
+        println("Lensa kamera terbuka dan sensor aktif")
+    }
+}
+interface Phone{
+    fun turnOn(){
+        println("Sinyal Seluler mencari jaringan")
+    }
+}
