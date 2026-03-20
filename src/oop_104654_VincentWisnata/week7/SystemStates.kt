9a8c33abdc50d1fdfba9685bc84062f694d48eea
Vincent Wisnata <81563427+Incentt@users.noreply.github.com>
2026-03-20 14:43:34 +0700

    week07: create ApiResponse sealed class

--- src/oop_104654_VincentWisnata/week7/SystemStates.kt
+++ src/oop_104654_VincentWisnata/week7/SystemStates.kt
@@ -1,5 +1,13 @@
 package oop_104654_VincentWisnata.week7
 
+import javax.annotation.processing.Messager
+
 enum class SystemStates {
     STARTING, RUNNING, STOPPED
+}
+
+sealed class ApiResponse{
+    data class Success(val data: String) : ApiResponse()
+    data class Error(val message: String) : ApiResponse()
+    object Loading : ApiResponse()
 }
