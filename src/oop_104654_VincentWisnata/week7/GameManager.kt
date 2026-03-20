41dc022dc62a081639692edc73d5089d9bd13f86
Vincent Wisnata <81563427+Incentt@users.noreply.github.com>
2026-03-20 14:47:34 +0700

    week07: (task) create GameManager singleton

---
+++ src/oop_104654_VincentWisnata/week7/GameManager.kt
@@ -0,0 +1,9 @@
+package oop_104654_VincentWisnata.week7
+
+object GameManager {
+    var isGameRunning: Boolean = false
+    fun startGame(){
+        isGameRunning = true
+        println("Game sudah berjalan")
+    }
+}
