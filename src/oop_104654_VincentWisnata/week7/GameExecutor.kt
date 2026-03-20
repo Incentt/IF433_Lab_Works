5124edd283929be43875f4e828fd9ecade13abd8
Vincent Wisnata <81563427+Incentt@users.noreply.github.com>
2026-03-20 15:02:55 +0700

    week07: (task) implement exhaustive processEvent

---
+++ src/oop_104654_VincentWisnata/week7/GameExecutor.kt
@@ -0,0 +1,13 @@
+package oop_104654_VincentWisnata.week7
+
+class GameExecutor {
+    fun processEvent(event: BattleState){
+        when(event){
+            is BattleState.MonsterEcounter -> println("Ecounter dengan $event.monsterName")
+            is BattleState.GameOver -> println("Game over: $event.reason")
+            is BattleState.LootDropped -> println("LootDropped $event.reason")
+            is BattleState.SafeZone -> println("Safezone ")
+            else -> println("Loading")
+        }
+    }
+}
