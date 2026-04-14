611d81ec03abbc48957498f5a37dca79ae40209d
Vincent Wisnata <81563427+Incentt@users.noreply.github.com>
2026-04-14 15:09:37 +0700

    week08: (task) create JavaPaymentService for payment interop

---
+++ src/oop_104654_VincentWisnata/week8/JavaPaymentService.java
@@ -0,0 +1,7 @@
+package oop_104654_VincentWisnata.week8;
+
+public class JavaPaymentService {
+    public static String processPayment(String productId){
+        return "TRX-" + productId + "-SUCCESS";
+    }
+}
