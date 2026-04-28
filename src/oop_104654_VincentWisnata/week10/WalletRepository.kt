2329bb225a23f667c19af7b6f819d19d26d880f1
Vincent Wisnata <81563427+Incentt@users.noreply.github.com>
2026-04-28 14:51:30 +0700

    week10: (task) implement search logic in repository

--- src/oop_104654_VincentWisnata/week10/WalletRepository.kt
+++ src/oop_104654_VincentWisnata/week10/WalletRepository.kt
@@ -1,11 +1,23 @@
 package oop_104654_VincentWisnata.week10
 
-class WalletRepository<T>{
+interface HasName {
+    val name: String
+}
+
+class WalletRepository<T : HasName> {
     private val items = mutableListOf<T>()
-    fun add(item: T){
+
+    fun add(item: T) {
         items.add(item)
     }
+
     fun getAll(): List<T> {
         return items
+    }
+
+    fun findByName(name: String): List<T> {
+        return items.filter {
+            it.name.contains(name, ignoreCase = true)
+        }
     }
 }
