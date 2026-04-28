4f8d1ad0a077b87d2a2db262585c92fbb26b3465
Vincent Wisnata <81563427+Incentt@users.noreply.github.com>
2026-04-28 14:59:22 +0700

    week10: (task) initialize coin repository

---
+++ src/oop_104654_VincentWisnata/week10/CryptoDashboard.kt
@@ -0,0 +1,5 @@
+package oop_104654_VincentWisnata.week10
+
+fun main(){
+    val coinRepo = WalletRepository<Coin>()
+}
--- src/oop_104654_VincentWisnata/week10/WalletModels.kt
+++ src/oop_104654_VincentWisnata/week10/WalletModels.kt
@@ -1,4 +1,7 @@
 package oop_104654_VincentWisnata.week10
 
-data class Coin(val name: String, val balance: Double)
+data class Coin(
+    override val name: String,
+    val balance: Double
+) : HasName
 data class Transaction(val id: String, val amount: Double)
