f28c5d98ebfc0a998d21b695d34879cba0ab2170
Vincent Wisnata <81563427+Incentt@users.noreply.github.com>
2026-05-05 14:49:42 +0700

    week11: (task) create extension function for SmartDevice

--- src/oop_104654_VincentWisnata/week11/SmartHomeModels.kt
+++ src/oop_104654_VincentWisnata/week11/SmartHomeModels.kt
@@ -1,4 +1,14 @@
 package oop_104654_VincentWisnata.week11
 
-data class SmartDevice(var name: String, var category:
-String, var isOnline: Boolean = false, var powerLoad: Int = 0)
+data class SmartDevice(
+    var name: String,
+    var category: String,
+    var isOnline: Boolean = false,
+    var powerLoad: Int = 0
+)
+
+fun SmartDevice.diagnose(): String {
+    return "[DIAGNOSTIK] $name | Kategori: $category | " +
+            "Status: ${if (isOnline) "Online" else "Offline"} | " +
+            "Daya: $powerLoad Watt"
+}
