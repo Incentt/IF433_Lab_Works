6ee4c03e3f1fbd07a512413d6f97efb7c2792833
Vincent Wisnata <81563427+Incentt@users.noreply.github.com>
2026-05-05 14:36:40 +0700

    week11: test extension functions in main

---
+++ src/oop_104654_VincentWisnata/week11/Main.kt
@@ -0,0 +1,10 @@
+package oop_104654_VincentWisnata.week11
+
+fun main(){
+    println("=== TEST EXTENSION FUNCTION ===")
+    println("Alex".addGreeting())
+    println("Hi".repeatTimes(3))
+
+    val text: String? = null
+println("Apakah null/empty? ${text.isNullOrEmptyCustom()}" )
+}
