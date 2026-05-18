fa41f64af7f3b5224f604f59a9a4853e925adb10
Vincent Wisnata <81563427+Incentt@users.noreply.github.com>
2026-05-19 01:49:56 +0700

    week12: implement onSuccess and onFailure chaining

--- src/oop_104654_VincentWisnata/week12/RunCatchingExample.kt
+++ src/oop_104654_VincentWisnata/week12/RunCatchingExample.kt
@@ -5,9 +5,17 @@ fun main(){
     val result: Result<Int> = runCatching {
         "42X".toInt()
     }
-    
+
     val safeValue = result.getOrElse { -1 }
     println("Safe Value (getorelse): $safeValue")
     val recovered = result.recover { 0 }.getOrNull()
     println("Recovered value: $recovered")
+
+    runCatching {
+        "100".toInt()
+    }.onSuccess { v->
+        println("Berhasl dikonversi: $v")
+    }.onFailure { e->
+        println("Gagal konversi: ${e.message}")
+    }
 }
