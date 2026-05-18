16d1bc0f1ee0ed023975e59350f30a29415f4714
Vincent Wisnata <81563427+Incentt@users.noreply.github.com>
2026-05-19 02:00:57 +0700

    week12: (task) handle feeding execution with multiple catch

--- src/oop_104654_VincentWisnata/week12/SmartFeeder.kt
+++ src/oop_104654_VincentWisnata/week12/SmartFeeder.kt
@@ -15,4 +15,14 @@ fun dispenseKibble(requestedGram: Int, availableGram: Int, isJammed: Boolean): I
 }
 fun main(){
     var currentKibbleStock = 50.0;
+    try {
+        dispenseKibble( 80, currentKibbleStock, false)
+    }catch (e: FoodEmptyException){
+        println(e.message)
+    }catch (e: DispenserJamException){
+        println(e.message)
+    }catch (e: Exception){
+        println("Something went wrong ${e.message}")
+    }
+
 }
