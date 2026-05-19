046131f172af05093ec84e34e05476c1bad20191
Vincent Wisnata <81563427+Incentt@users.noreply.github.com>
2026-05-19 14:15:43 +0700

    week13: implement toCsv and fromCsv logic

--- src/oop_104654_VincentWisnata/week13/StudentSerialization.kt
+++ src/oop_104654_VincentWisnata/week13/StudentSerialization.kt
@@ -2,3 +2,9 @@ package oop_104654_VincentWisnata.week13
 
 data class Student(var nama: String, val age: Int, val gpa: Double)
 
+fun Student.toCsv(): String = "$nama, $age, $gpa"
+
+fun fromCSV(line: String): Student {
+    val parts = line.split(",")
+    return Student(parts[0], parts[1].toInt(), parts[2].toDouble())
+}
