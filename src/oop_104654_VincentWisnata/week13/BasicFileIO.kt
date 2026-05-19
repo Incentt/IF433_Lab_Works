f8c8ddd4b4fef2e9097fe0856120f266b9cdfc12
Vincent Wisnata <81563427+Incentt@users.noreply.github.com>
2026-05-19 14:05:52 +0700

    week13: implement basic writeText function

---
+++ src/oop_104654_VincentWisnata/week13/BasicFileIO.kt
@@ -0,0 +1,12 @@
+package oop_104654_VincentWisnata.week13
+
+import java.io.File
+
+fun main(){
+    val src = "/Users/incent/Documents/UMN/IF433_Lab_Works/IF433_Lab_Works/src/oop_104654_VincentWisnata/week13/"
+
+    println("=== TEST WRITE TEXT ===")
+    val file = File("${src}notes.txt")
+    file.writeText("Line1: inisialisasi sistem \n")
+    println("File berhasil dibuat")
+}
