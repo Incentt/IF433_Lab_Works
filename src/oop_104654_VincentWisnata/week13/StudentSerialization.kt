15d7d6a34b9832764a05f6633414222e3fc55f8e
Vincent Wisnata <81563427+Incentt@users.noreply.github.com>
2026-05-19 14:27:00 +0700

    week13: test complete student serialization pipeline

--- src/oop_104654_VincentWisnata/week13/StudentSerialization.kt
+++ src/oop_104654_VincentWisnata/week13/StudentSerialization.kt
@@ -5,17 +5,36 @@ import java.io.FileNotFoundException
 
 data class Student(var nama: String, val age: Int, val gpa: Double)
 
-fun Student.toCsv(): String = "$nama, $age, $gpa"
+fun Student.toCsv(): String = "$nama,$age,$gpa"
 
 fun fromCSV(line: String): Student {
     val parts = line.split(",")
     return Student(parts[0], parts[1].toInt(), parts[2].toDouble())
 }
+fun saveStudents(students: List<Student>, path: String) {
+    File("$path").writeText(students.joinToString("\n"){
+        it.toCsv()
+    }
+    )
+}
+
 fun loadStudents(path: String): List<Student> {
     return try{
-        File(path).readLines().map { fromCSV(it)}
+            File(path).readLines().map { fromCSV(it)}
     }catch(e: FileNotFoundException){
         println("EROR: File tidak ditemukan")
         emptyList()
     }
+}
+fun main (){
+    val src = "/Users/incent/Documents/UMN/IF433_Lab_Works/IF433_Lab_Works/src/oop_104654_VincentWisnata/week13/"
+    val Students = listOf(
+        Student("ALice",20, 3.0),
+        Student("Bob", 22, 3.5)
+    )
+    saveStudents(Students, "$src/students.csv")
+
+    val loaded = loadStudents("$src/students.csv")
+    println("=== LOADED STUDENT DATA ===")
+    loaded.forEach { println(it) }
 }
